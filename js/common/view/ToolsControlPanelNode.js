e160ba84e97d1cd499618c3abe1359b7f223b3ce
MLearner05 <andrey.zelenkov@mlearner.com>
2014-10-13 20:46:55 +0400

    refactor tools control panel

--- js/common/view/ToolsControlPanelNode.js
+++ js/common/view/ToolsControlPanelNode.js
@@ -11,7 +11,6 @@ define( function( require ) {
   'use strict';
 
   // modules
-  var CheckBox = require( 'SUN/CheckBox' );
   var inherit = require( 'PHET_CORE/inherit' );
   var HStrut = require( 'SUN/HStrut' );
   var Node = require( 'SCENERY/nodes/Node' );
@@ -19,7 +18,7 @@ define( function( require ) {
   var PendulumLabConstants = require( 'PENDULUM_LAB/common/PendulumLabConstants' );
   var PhetFont = require( 'SCENERY_PHET/PhetFont' );
   var Text = require( 'SCENERY/nodes/Text' );
-  var VBox = require( 'SCENERY/nodes/VBox' );
+  var VerticalCheckBoxGroup = require( 'SUN/VerticalCheckBoxGroup' );
 
   // strings
   var RulerString = require( 'string!PENDULUM_LAB/ruler' );
@@ -28,57 +27,48 @@ define( function( require ) {
 
   // constants
   var FONT = new PhetFont( 11 );
-  var CHECK_BOX_OPTIONS = {
-    boxWidth: 15
-  };
   var PANEL_WIDTH = 122;
 
   /**
    * {Property} isRulerProperty - property to control visibility of ruler
    * {Property} isStopwatchProperty - property to control visibility of stopwatch
-   * {Property} isPeriodTrace - property to control visibility of period trace tool
+   * {Property} isPeriodTraceProperty - property to control visibility of period trace tool
    * {Object} options for tools control panel node
    * @constructor
    */
-  function ToolsControlPanelNode( isRulerProperty, isStopwatchProperty, isPeriodTrace, options ) {
+  function ToolsControlPanelNode( isRulerProperty, isStopwatchProperty, isPeriodTraceProperty, options ) {
     Panel.call( this,
       new Node( {children: [
-        // necessary to expand panel
-        new HStrut( PANEL_WIDTH ),
+          // necessary to expand panel
+          new HStrut( PANEL_WIDTH ),
 
-        // contains checkboxes
-        new VBox( {
-          align: 'left',
-          spacing: 5,
-          children: [
-            // ruler checkbox
-            new CheckBox(
-              new Text( RulerString, {font: FONT} ),
-              isRulerProperty,
-              CHECK_BOX_OPTIONS
-            ),
-
-            // stopwatch checkbox
-            new CheckBox(
-              new Text( StopwatchString, {font: FONT} ),
-              isStopwatchProperty,
-              CHECK_BOX_OPTIONS
-            ),
-
-            // period trace
-            new CheckBox(
-              new Text( PeriodTraceString, {font: FONT} ),
-              isPeriodTrace,
-              CHECK_BOX_OPTIONS
-            )
-          ]} )
-      ]} ), _.extend( {
+          new VerticalCheckBoxGroup( [
+            {
+              content: new Text( RulerString, {font: FONT} ),
+              property: isRulerProperty
+            },
+            {
+              content: new Text( StopwatchString, {font: FONT} ),
+              property: isStopwatchProperty
+            },
+            {
+              content: new Text( PeriodTraceString, {font: FONT} ),
+              property: isPeriodTraceProperty
+            }
+          ], {spacing: 5, boxWidth: 15} )
+        ]
+        }
+      ),
+      _.extend( {
         cornerRadius: 5,
         fill: PendulumLabConstants.CONTROL_PANEL_BACKGROUND_COLOR,
         xMargin: 10,
         yMargin: 10
-      }, options ) );
+      }, options )
+    )
+    ;
   }
 
   return inherit( Panel, ToolsControlPanelNode );
-} );
+} )
+;
