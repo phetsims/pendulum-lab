06062b1c25e2bcf321ee7062680d585db71d87fe
MLearner05 <andrey.zelenkov@mlearner.com>
2014-11-13 16:36:53 +0300

    added fading for trace path

--- js/common/view/PeriodTraceNode.js
+++ js/common/view/PeriodTraceNode.js
@@ -15,6 +15,7 @@ define( function( require ) {
   var Path = require( 'SCENERY/nodes/Path' );
   var PendulumLabConstants = require( 'PENDULUM_LAB/common/PendulumLabConstants' );
   var Shape = require( 'KITE/Shape' );
+  var Timer = require( 'JOIST/Timer' );
 
   // constants
   var EMPTY_SHAPE = new Shape();
@@ -36,6 +37,7 @@ define( function( require ) {
     pendulumModels.forEach( function( pendulumModel ) {
       var pathPointsStorage = [], // array for store passed points
         anticlockwise, // trace direction flag
+        intervalId, // interval id for fading timer
         traceLength, // trace initial length
         isCompleted = false; // flag to control completing of trace
 
@@ -47,12 +49,14 @@ define( function( require ) {
         pathNode.setShape( EMPTY_SHAPE );
         pathPointsStorage = [];
         isCompleted = false;
+        Timer.clearInterval( intervalId );
+        pathNode.opacity = 1;
       };
 
       var updateShape = function() {
         var shape = new Shape();
 
-        if ( pathPointsStorage.length > 0 && !isCompleted ) {
+        if ( pathPointsStorage.length > 0 ) {
           // draw first arc
           if ( pathPointsStorage.length > 1 ) {
             shape.arc( 0, 0, traceLength, 0, pathPointsStorage[1].angle, pathPointsStorage[0].anticlockwise );
@@ -67,6 +71,7 @@ define( function( require ) {
               if ( pathPointsStorage.length > 3 ) {
                 shape.arc( 0, 0, traceLength - 2 * TRACE_STEP, pathPointsStorage[2].angle, 0, pathPointsStorage[2].anticlockwise );
                 isCompleted = true;
+                fadeOutPath( 3 / 2 * Math.PI / Math.abs( pendulumModel.omega ) * 10 );
               }
               else {
                 shape.arc( 0, 0, traceLength - 2 * TRACE_STEP, pathPointsStorage[2].angle, pendulumModel.angle, pathPointsStorage[2].anticlockwise );
@@ -83,6 +88,17 @@ define( function( require ) {
         }
       };
 
+      var fadeOutPath = function( tickTime ) {
+        intervalId = Timer.setInterval( function() {
+          pathNode.opacity -= 0.01;
+          if ( pathNode.opacity <= 0 ) {
+            pathNode.visible = false;
+            pathNode.opacity = 1;
+            Timer.clearInterval( intervalId );
+          }
+        }, tickTime );
+      };
+
       // update visibility of path node
       isPeriodTraceVisibleProperty.link( function( isPeriodTraceVisible ) {
         pathNode.visible = isPeriodTraceVisible;
@@ -91,7 +107,7 @@ define( function( require ) {
 
       // update path shape
       pendulumModel.property( 'angle' ).link( function( newAngle, oldAngle ) {
-        if ( pathNode.visible && !pendulumModel.isUserControlled ) {
+        if ( pathNode.visible && !pendulumModel.isUserControlled && !isCompleted ) {
           // first point
           if ( pathPointsStorage.length === 0 && newAngle * oldAngle < 0 ) {
             anticlockwise = newAngle < 0;
@@ -112,6 +128,7 @@ define( function( require ) {
           else if ( pathPointsStorage.length === 3 && newAngle * oldAngle < 0 ) {
             pathPointsStorage.push( {anticlockwise: anticlockwise} );
           }
+
           updateShape();
         }
       } );
