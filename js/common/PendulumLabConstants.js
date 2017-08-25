94acb4979e5604c733264a4be4affccc4c04ad9d
Jonathan Olson <olsonsjc@gmail.com>
2017-08-25 09:17:18 -0600

    Increase in readout font, see https://github.com/phetsims/pendulum-lab/issues/179

--- js/common/PendulumLabConstants.js
+++ js/common/PendulumLabConstants.js
@@ -40,7 +40,7 @@ define( function( require ) {
     TITLE_FONT: new PhetFont( 14 ),
     TITLE_FONT_BOLD: new PhetFont( { size: 14, weight: 'bold' } ),
     TICK_FONT: new PhetFont( 12 ),
-    READOUT_FONT: new PhetFont( 12 ),
+    READOUT_FONT: new PhetFont( 14 ),
     RULER_FONT: new PhetFont( 10 ),
     PERIOD_TIMER_TITLE_FONT: new PhetFont( 14 ),
     PERIOD_TIMER_READOUT_FONT: new PhetFont( 14 ),
