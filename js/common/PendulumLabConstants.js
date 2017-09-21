01a2fe57276c2983e986e1445e0eb535403d817b
samreid <reids@colorado.edu>
2017-09-21 10:19:48 -0600

    Updated copyright dates, see https://github.com/phetsims/chipper/issues/403

--- js/common/PendulumLabConstants.js
+++ js/common/PendulumLabConstants.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2015, University of Colorado Boulder
+// Copyright 2014-2017, University of Colorado Boulder
 
 /**
  * Constants used in multiple locations within the 'Pendulum Lab' simulation.
--- js/common/view/PlaybackControlsNode.js
+++ js/common/view/PlaybackControlsNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2015, University of Colorado Boulder
+// Copyright 2014-2017, University of Colorado Boulder
 
 /**
  * Pendula system control panel node in 'Pendulum Lab' simulation.
