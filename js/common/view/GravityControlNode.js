c44e6bceae4e68863288927616b7b1615d674031
samreid <reids@colorado.edu>
2018-11-19 12:55:10 -0700

    Update copyright dates, see https://github.com/phetsims/chipper/issues/711

--- js/common/view/GravityControlNode.js
+++ js/common/view/GravityControlNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2017, University of Colorado Boulder
+// Copyright 2014-2018, University of Colorado Boulder
 
 /**
  * Gravity slider and body list node in 'Pendulum lab' simulation.
--- js/common/view/StopButton.js
+++ js/common/view/StopButton.js
@@ -1,4 +1,4 @@
-// Copyright 2017, University of Colorado Boulder
+// Copyright 2017-2018, University of Colorado Boulder
 
 /**
  * Stop button node in 'Pendulum Lab' simulation.
