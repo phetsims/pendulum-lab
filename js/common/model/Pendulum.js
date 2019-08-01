5229b2a947a798fa21ab0d123804ea9c3959142a
zepumph <michael.kauzmann@colorado.edu>
2019-07-31 18:21:00 -0800

    rename validators -> parameters, https://github.com/phetsims/axon/issues/257

--- js/common/model/Pendulum.js
+++ js/common/model/Pendulum.js
@@ -101,10 +101,10 @@ define( function( require ) {
     this.frictionProperty = frictionProperty;
 
     // @public
-    this.stepEmitter = new Emitter( { validators: [ { valueType: 'number' } ] } );
+    this.stepEmitter = new Emitter( { parameters: [ { valueType: 'number' } ] } );
     this.userMovedEmitter = new Emitter();
-    this.crossingEmitter = new Emitter( { validators: [ { valueType: 'number' }, { valueType: 'boolean' } ] } );
-    this.peakEmitter = new Emitter( { validators: [ { valueType: 'number' } ] } );
+    this.crossingEmitter = new Emitter( { parameters: [ { valueType: 'number' }, { valueType: 'boolean' } ] } );
+    this.peakEmitter = new Emitter( { parameters: [ { valueType: 'number' } ] } );
     this.resetEmitter = new Emitter();
 
     // default color for this pendulum
