c94263d9942e3d0a5ae466ea9447ac7b757504ed
pixelzoom <cmalley@pixelzoom.com>
2020-09-29 17:34:40 -0600

    convert RectangularPushButton subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/common/view/StopButton.js
+++ js/common/view/StopButton.js
@@ -6,31 +6,29 @@
  * @author Andrey Zelenkov (Mlearner)
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import StopSignNode from '../../../../scenery-phet/js/StopSignNode.js';
 import RectangularPushButton from '../../../../sun/js/buttons/RectangularPushButton.js';
 import pendulumLab from '../../pendulumLab.js';
 
-/**
- * @constructor
- *
- * @param {Object} [options]
- */
-function StopButton( options ) {
-  RectangularPushButton.call( this, merge( {
-    xMargin: 7,
-    yMargin: 3,
-    touchAreaXDilation: 6,
-    touchAreaYDilation: 6,
-    baseColor: 'rgb( 231, 232, 233 )',
-    content: new StopSignNode( {
-      scale: 0.4
-    } )
-  }, options ) );
+class StopButton extends RectangularPushButton {
+
+  /**
+   * @param {Object} [options]
+   */
+  constructor( options ) {
+    super( merge( {
+      xMargin: 7,
+      yMargin: 3,
+      touchAreaXDilation: 6,
+      touchAreaYDilation: 6,
+      baseColor: 'rgb( 231, 232, 233 )',
+      content: new StopSignNode( {
+        scale: 0.4
+      } )
+    }, options ) );
+  }
 }
 
 pendulumLab.register( 'StopButton', StopButton );
-
-inherit( RectangularPushButton, StopButton );
 export default StopButton;
