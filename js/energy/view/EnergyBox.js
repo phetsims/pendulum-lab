9b10c1f366f13cd9b63a8c21f5b5a50ef5293f0a
pixelzoom <cmalley@pixelzoom.com>
2020-09-28 23:04:30 -0600

    convert AccordionBox subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/energy/view/EnergyBox.js
+++ js/energy/view/EnergyBox.js
@@ -10,7 +10,6 @@ import DerivedProperty from '../../../../axon/js/DerivedProperty.js';
 import DynamicProperty from '../../../../axon/js/DynamicProperty.js';
 import Range from '../../../../dot/js/Range.js';
 import BarChartNode from '../../../../griddle/js/BarChartNode.js';
-import inherit from '../../../../phet-core/js/inherit.js';
 import merge from '../../../../phet-core/js/merge.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import InfoButton from '../../../../scenery-phet/js/buttons/InfoButton.js';
@@ -37,231 +36,229 @@ const legendThermalEnergyAbbreviationString = pendulumLabStrings.legend.thermalE
 const legendTotalEnergyAbbreviationString = pendulumLabStrings.legend.totalEnergyAbbreviation;
 const pendulumMassPatternString = pendulumLabStrings.pendulumMassPattern;
 
+class EnergyBox extends AccordionBox {
 
-/**
- * @constructor
- *
- * @param {EnergyModel} model
- * @param {Property.<number>} chartHeightProperty
- * @param {Object} [options]
- */
-function EnergyBox( model, chartHeightProperty, options ) {
-  options = merge( {}, PendulumLabConstants.BOX_OPTIONS, {
-    expandedProperty: model.isEnergyBoxExpandedProperty,
-    buttonXMargin: 10,
-    buttonYMargin: 6,
-    titleNode: new Text( energyGraphString, {
-      font: PendulumLabConstants.TITLE_FONT,
-      maxWidth: 110
-    } ),
-    titleAlignX: 'left',
-    titleXMargin: 10,
-    contentYSpacing: 0,
-    resize: true
-  }, options );
+  /**
+   * @param {EnergyModel} model
+   * @param {Property.<number>} chartHeightProperty
+   * @param {Object} [options]
+   */
+  constructor( model, chartHeightProperty, options ) {
 
-  const headerText = new Text( '', {
-    font: PendulumLabConstants.ENERGY_HEADER_FONT,
-    maxWidth: 122
-  } );
+    options = merge( {}, PendulumLabConstants.BOX_OPTIONS, {
+      expandedProperty: model.isEnergyBoxExpandedProperty,
+      buttonXMargin: 10,
+      buttonYMargin: 6,
+      titleNode: new Text( energyGraphString, {
+        font: PendulumLabConstants.TITLE_FONT,
+        maxWidth: 110
+      } ),
+      titleAlignX: 'left',
+      titleXMargin: 10,
+      contentYSpacing: 0,
+      resize: true
+    }, options );
 
-  model.activeEnergyPendulumProperty.link( function( pendulum ) {
-    headerText.text = StringUtils.fillIn( pendulumMassPatternString, {
-      pendulumNumber: '' + ( pendulum.index + 1 )
+    const headerText = new Text( '', {
+      font: PendulumLabConstants.ENERGY_HEADER_FONT,
+      maxWidth: 122
     } );
-    headerText.fill = pendulum.color;
-  } );
 
-  const kineticEnergyProperty = new DynamicProperty( model.activeEnergyPendulumProperty, { derive: 'kineticEnergyProperty' } );
-  const potentialEnergyProperty = new DynamicProperty( model.activeEnergyPendulumProperty, { derive: 'potentialEnergyProperty' } );
-  const thermalEnergyProperty = new DynamicProperty( model.activeEnergyPendulumProperty, {
-    bidirectional: true,
-    derive: 'thermalEnergyProperty'
-  } );
-
-  const clearThermalButton = new MoveToTrashButton( {
-    arrowColor: PendulumLabConstants.THERMAL_ENERGY_COLOR,
-    listener: thermalEnergyProperty.reset.bind( thermalEnergyProperty ),
-    scale: 0.72
-  } );
-  thermalEnergyProperty.link( function( thermalEnergy ) {
-    clearThermalButton.enabled = thermalEnergy !== 0;
-  } );
-
-  const chartRangeProperty = new DerivedProperty( [ chartHeightProperty ], function( chartHeight ) {
-    return new Range( 0, chartHeight );
-  } );
-  const kineticBarEntry = {
-    property: kineticEnergyProperty,
-    color: PendulumLabConstants.KINETIC_ENERGY_COLOR
-  };
-  const potentialBarEntry = {
-    property: potentialEnergyProperty,
-    color: PendulumLabConstants.POTENTIAL_ENERGY_COLOR
-  };
-  const thermalBarEntry = {
-    property: thermalEnergyProperty,
-    color: PendulumLabConstants.THERMAL_ENERGY_COLOR
-  };
-  const barChartNode = new BarChartNode( [
-    {
-      entries: [ kineticBarEntry ],
-      labelString: legendKineticEnergyAbbreviationString
-    },
-    {
-      entries: [ potentialBarEntry ],
-      labelString: legendPotentialEnergyAbbreviationString
-    },
-    {
-      entries: [ thermalBarEntry ],
-      labelString: legendThermalEnergyAbbreviationString,
-      labelNode: clearThermalButton
-    },
-    {
-      entries: [ kineticBarEntry, potentialBarEntry, thermalBarEntry ],
-      labelString: legendTotalEnergyAbbreviationString,
-      offScaleArrowFill: '#bbb'
-    }
-  ], chartRangeProperty, {
-    barOptions: {
-      // Apply a scaling correction (so that energyZoomProperty=1 corresponds to 40 * the actual energy amount)
-      scaleProperty: new DerivedProperty( [ model.energyZoomProperty ], function( energyZoom ) { return 40 * energyZoom; } )
-    }
-  } );
-
-  function barChartUpdate() {
-    if ( model.isEnergyBoxExpandedProperty.value ) {
-      barChartNode.update();
-    }
-  }
-
-  kineticEnergyProperty.lazyLink( barChartUpdate );
-  potentialEnergyProperty.lazyLink( barChartUpdate );
-  thermalEnergyProperty.lazyLink( barChartUpdate );
-  model.energyZoomProperty.lazyLink( barChartUpdate );
-  chartRangeProperty.lazyLink( barChartUpdate );
-  model.isEnergyBoxExpandedProperty.lazyLink( barChartUpdate );
-  barChartUpdate();
-
-  const content = new VBox( {
-    spacing: 4,
-    children: [
-      headerText,
-      barChartNode
-    ]
-  } );
-
-  function createRadioButton( pendulum ) {
-    const label = new Text( pendulum.index + 1, {
-      font: PendulumLabConstants.TITLE_FONT
+    model.activeEnergyPendulumProperty.link( function( pendulum ) {
+      headerText.text = StringUtils.fillIn( pendulumMassPatternString, {
+        pendulumNumber: '' + ( pendulum.index + 1 )
+      } );
+      headerText.fill = pendulum.color;
     } );
-    const button = new AquaRadioButton( model.activeEnergyPendulumProperty, pendulum, label, {
-      radius: label.height / 2.2,
-      xSpacing: 3
-    } );
-    button.touchArea = button.localBounds.dilatedXY( 10, 5 );
-    return button;
-  }
 
-  function createZoomButton( isIn ) {
-    return new ZoomButton( merge( {
-      in: isIn,
+    const kineticEnergyProperty = new DynamicProperty( model.activeEnergyPendulumProperty, { derive: 'kineticEnergyProperty' } );
+    const potentialEnergyProperty = new DynamicProperty( model.activeEnergyPendulumProperty, { derive: 'potentialEnergyProperty' } );
+    const thermalEnergyProperty = new DynamicProperty( model.activeEnergyPendulumProperty, {
+      bidirectional: true,
+      derive: 'thermalEnergyProperty'
+    } );
+
+    const clearThermalButton = new MoveToTrashButton( {
+      arrowColor: PendulumLabConstants.THERMAL_ENERGY_COLOR,
+      listener: thermalEnergyProperty.reset.bind( thermalEnergyProperty ),
+      scale: 0.72
+    } );
+    thermalEnergyProperty.link( function( thermalEnergy ) {
+      clearThermalButton.enabled = thermalEnergy !== 0;
+    } );
+
+    const chartRangeProperty = new DerivedProperty( [ chartHeightProperty ], function( chartHeight ) {
+      return new Range( 0, chartHeight );
+    } );
+    const kineticBarEntry = {
+      property: kineticEnergyProperty,
+      color: PendulumLabConstants.KINETIC_ENERGY_COLOR
+    };
+    const potentialBarEntry = {
+      property: potentialEnergyProperty,
+      color: PendulumLabConstants.POTENTIAL_ENERGY_COLOR
+    };
+    const thermalBarEntry = {
+      property: thermalEnergyProperty,
+      color: PendulumLabConstants.THERMAL_ENERGY_COLOR
+    };
+    const barChartNode = new BarChartNode( [
+      {
+        entries: [ kineticBarEntry ],
+        labelString: legendKineticEnergyAbbreviationString
+      },
+      {
+        entries: [ potentialBarEntry ],
+        labelString: legendPotentialEnergyAbbreviationString
+      },
+      {
+        entries: [ thermalBarEntry ],
+        labelString: legendThermalEnergyAbbreviationString,
+        labelNode: clearThermalButton
+      },
+      {
+        entries: [ kineticBarEntry, potentialBarEntry, thermalBarEntry ],
+        labelString: legendTotalEnergyAbbreviationString,
+        offScaleArrowFill: '#bbb'
+      }
+    ], chartRangeProperty, {
+      barOptions: {
+        // Apply a scaling correction (so that energyZoomProperty=1 corresponds to 40 * the actual energy amount)
+        scaleProperty: new DerivedProperty( [ model.energyZoomProperty ], function( energyZoom ) { return 40 * energyZoom; } )
+      }
+    } );
+
+    function barChartUpdate() {
+      if ( model.isEnergyBoxExpandedProperty.value ) {
+        barChartNode.update();
+      }
+    }
+
+    kineticEnergyProperty.lazyLink( barChartUpdate );
+    potentialEnergyProperty.lazyLink( barChartUpdate );
+    thermalEnergyProperty.lazyLink( barChartUpdate );
+    model.energyZoomProperty.lazyLink( barChartUpdate );
+    chartRangeProperty.lazyLink( barChartUpdate );
+    model.isEnergyBoxExpandedProperty.lazyLink( barChartUpdate );
+    barChartUpdate();
+
+    const content = new VBox( {
+      spacing: 4,
+      children: [
+        headerText,
+        barChartNode
+      ]
+    } );
+
+    function createRadioButton( pendulum ) {
+      const label = new Text( pendulum.index + 1, {
+        font: PendulumLabConstants.TITLE_FONT
+      } );
+      const button = new AquaRadioButton( model.activeEnergyPendulumProperty, pendulum, label, {
+        radius: label.height / 2.2,
+        xSpacing: 3
+      } );
+      button.touchArea = button.localBounds.dilatedXY( 10, 5 );
+      return button;
+    }
+
+    function createZoomButton( isIn ) {
+      return new ZoomButton( merge( {
+        in: isIn,
+        listener: function() {
+          const zoomMultiplier = 1.3;
+          if ( isIn ) {
+            model.energyZoomProperty.value *= zoomMultiplier;
+          }
+          else {
+            model.energyZoomProperty.value /= zoomMultiplier;
+          }
+        }
+      }, {
+        baseColor: ColorConstants.LIGHT_BLUE,
+        radius: 7,
+        touchAreaXDilation: 5,
+        touchAreaYDilation: 5
+      } ) );
+    }
+
+    const radioButtonOne = createRadioButton( model.pendula[ 0 ] );
+    const radioButtonTwo = createRadioButton( model.pendula[ 1 ] );
+
+    // no need to unlink, present for the lifetime of the sim
+    model.numberOfPendulaProperty.link( function( numberOfPendula ) {
+      if ( numberOfPendula === 1 ) {
+        model.activeEnergyPendulumProperty.value = model.pendula[ 0 ];
+        radioButtonTwo.setEnabled( false );
+      }
+      else if ( numberOfPendula === 2 ) {
+        radioButtonTwo.setEnabled( true );
+      }
+    } );
+
+    const panel = new Panel( content, {
+      cornerRadius: PendulumLabConstants.PANEL_CORNER_RADIUS
+    } );
+
+    const zoomOutButton = createZoomButton( false );
+    const zoomInButton = createZoomButton( true );
+
+    let energyDialog; // lazily created
+
+    const infoButton = new InfoButton( {
+      iconFill: 'rgb( 41, 106, 163 )',
+      maxHeight: 1.1 * zoomInButton.height,
+      left: panel.left,
+      centerY: zoomOutButton.centerY,
       listener: function() {
-        const zoomMultiplier = 1.3;
-        if ( isIn ) {
-          model.energyZoomProperty.value *= zoomMultiplier;
+        // Lazy creation.
+        if ( !energyDialog ) {
+          energyDialog = new EnergyLegendDialog();
         }
-        else {
-          model.energyZoomProperty.value /= zoomMultiplier;
-        }
-      }
-    }, {
-      baseColor: ColorConstants.LIGHT_BLUE,
-      radius: 7,
-      touchAreaXDilation: 5,
+        energyDialog.show();
+      },
+      touchAreaXDilation: 10,
       touchAreaYDilation: 5
-    } ) );
+    } );
+
+    const radioButtonBox = new HBox( {
+      spacing: 20,
+      children: [
+        radioButtonOne,
+        radioButtonTwo
+      ]
+    } );
+
+    // no need to unlink, present for the lifetime of the sim
+    model.numberOfPendulaProperty.link( function( numberOfPendula ) {
+      radioButtonBox.visible = numberOfPendula === 2;
+    } );
+
+    const boxContent = new VBox( {
+      spacing: 5,
+      excludeInvisibleChildrenFromBounds: false,
+      children: [
+        radioButtonBox,
+        panel,
+        new Node( {
+          children: [
+            infoButton,
+            new HBox( {
+              spacing: 10,
+              children: [
+                zoomOutButton,
+                zoomInButton
+              ],
+              right: panel.right
+            } )
+          ]
+        } )
+      ]
+    } );
+
+    super( new AlignBox( boxContent, { group: PendulumLabConstants.LEFT_CONTENT_ALIGN_GROUP } ), options );
   }
-
-  const radioButtonOne = createRadioButton( model.pendula[ 0 ] );
-  const radioButtonTwo = createRadioButton( model.pendula[ 1 ] );
-
-  // no need to unlink, present for the lifetime of the sim
-  model.numberOfPendulaProperty.link( function( numberOfPendula ) {
-    if ( numberOfPendula === 1 ) {
-      model.activeEnergyPendulumProperty.value = model.pendula[ 0 ];
-      radioButtonTwo.setEnabled( false );
-    }
-    else if ( numberOfPendula === 2 ) {
-      radioButtonTwo.setEnabled( true );
-    }
-  } );
-
-  const panel = new Panel( content, {
-    cornerRadius: PendulumLabConstants.PANEL_CORNER_RADIUS
-  } );
-
-  const zoomOutButton = createZoomButton( false );
-  const zoomInButton = createZoomButton( true );
-
-  let energyDialog; // lazily created
-
-  const infoButton = new InfoButton( {
-    iconFill: 'rgb( 41, 106, 163 )',
-    maxHeight: 1.1 * zoomInButton.height,
-    left: panel.left,
-    centerY: zoomOutButton.centerY,
-    listener: function() {
-      // Lazy creation.
-      if ( !energyDialog ) {
-        energyDialog = new EnergyLegendDialog();
-      }
-      energyDialog.show();
-    },
-    touchAreaXDilation: 10,
-    touchAreaYDilation: 5
-  } );
-
-  const radioButtonBox = new HBox( {
-    spacing: 20,
-    children: [
-      radioButtonOne,
-      radioButtonTwo
-    ]
-  } );
-
-  // no need to unlink, present for the lifetime of the sim
-  model.numberOfPendulaProperty.link( function( numberOfPendula ) {
-    radioButtonBox.visible = numberOfPendula === 2;
-  } );
-
-  const boxContent = new VBox( {
-    spacing: 5,
-    excludeInvisibleChildrenFromBounds: false,
-    children: [
-      radioButtonBox,
-      panel,
-      new Node( {
-        children: [
-          infoButton,
-          new HBox( {
-            spacing: 10,
-            children: [
-              zoomOutButton,
-              zoomInButton
-            ],
-            right: panel.right
-          } )
-        ]
-      } )
-    ]
-  } );
-
-  // add accordion box
-  AccordionBox.call( this, new AlignBox( boxContent, { group: PendulumLabConstants.LEFT_CONTENT_ALIGN_GROUP } ), options );
 }
 
 pendulumLab.register( 'EnergyBox', EnergyBox );
-
-inherit( AccordionBox, EnergyBox );
 export default EnergyBox;
