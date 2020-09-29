42d7a47da2f5a5681b576f64fdccde60dc8b135e
pixelzoom <cmalley@pixelzoom.com>
2020-09-28 22:26:03 -0600

    convert Dialog subclasses to ES6 class, https://github.com/phetsims/tasks/issues/1044

--- js/energy/view/EnergyLegendDialog.js
+++ js/energy/view/EnergyLegendDialog.js
@@ -6,7 +6,6 @@
  * @author Jonathan Olson <jonathan.olson@colorado.edu>
  */
 
-import inherit from '../../../../phet-core/js/inherit.js';
 import AlignBox from '../../../../scenery/js/nodes/AlignBox.js';
 import AlignGroup from '../../../../scenery/js/nodes/AlignGroup.js';
 import HBox from '../../../../scenery/js/nodes/HBox.js';
@@ -15,8 +14,8 @@ import Text from '../../../../scenery/js/nodes/Text.js';
 import VBox from '../../../../scenery/js/nodes/VBox.js';
 import Dialog from '../../../../sun/js/Dialog.js';
 import PendulumLabConstants from '../../common/PendulumLabConstants.js';
-import pendulumLabStrings from '../../pendulumLabStrings.js';
 import pendulumLab from '../../pendulumLab.js';
+import pendulumLabStrings from '../../pendulumLabStrings.js';
 
 const energyLegendString = pendulumLabStrings.energyLegend;
 const legendKineticEnergyAbbreviationString = pendulumLabStrings.legend.kineticEnergyAbbreviation;
@@ -28,67 +27,64 @@ const legendThermalEnergyString = pendulumLabStrings.legend.thermalEnergy;
 const legendTotalEnergyAbbreviationString = pendulumLabStrings.legend.totalEnergyAbbreviation;
 const legendTotalEnergyString = pendulumLabStrings.legend.totalEnergy;
 
-/**
- * @constructor
- */
-function EnergyLegendDialog() {
-  const abbreviationGroup = new AlignGroup();
-  const descriptionGroup = new AlignGroup();
+class EnergyLegendDialog extends Dialog {
+  constructor() {
+    const abbreviationGroup = new AlignGroup();
+    const descriptionGroup = new AlignGroup();
 
-  const content = new VBox( {
-    spacing: 15,
-    children: [
-      {
-        abbreviation: legendKineticEnergyAbbreviationString,
-        description: legendKineticEnergyString,
-        color: PendulumLabConstants.KINETIC_ENERGY_COLOR
-      }, {
-        abbreviation: legendPotentialEnergyAbbreviationString,
-        description: legendPotentialEnergyString,
-        color: PendulumLabConstants.POTENTIAL_ENERGY_COLOR
-      }, {
-        abbreviation: legendThermalEnergyAbbreviationString,
-        description: legendThermalEnergyString,
-        color: PendulumLabConstants.THERMAL_ENERGY_COLOR
-      }, {
-        abbreviation: legendTotalEnergyAbbreviationString,
-        description: legendTotalEnergyString,
-        color: PendulumLabConstants.TOTAL_ENERGY_COLOR
-      }
-    ].map( function( itemData ) {
-      return new HBox( {
-        spacing: 20,
-        children: [
-          new AlignBox( new RichText( itemData.abbreviation, {
-            font: PendulumLabConstants.LEGEND_ABBREVIATION_FONT,
-            fill: itemData.color,
-            maxWidth: 100
-          } ), {
-            group: abbreviationGroup,
-            xAlign: 'left'
-          } ),
-          new AlignBox( new Text( itemData.description, {
-            font: PendulumLabConstants.LEGEND_DESCRIPTION_FONT
-          } ), {
-            group: descriptionGroup,
-            xAlign: 'left',
-            maxWidth: 500
-          } )
-        ]
-      } );
-    } )
-  } );
+    const content = new VBox( {
+      spacing: 15,
+      children: [
+        {
+          abbreviation: legendKineticEnergyAbbreviationString,
+          description: legendKineticEnergyString,
+          color: PendulumLabConstants.KINETIC_ENERGY_COLOR
+        }, {
+          abbreviation: legendPotentialEnergyAbbreviationString,
+          description: legendPotentialEnergyString,
+          color: PendulumLabConstants.POTENTIAL_ENERGY_COLOR
+        }, {
+          abbreviation: legendThermalEnergyAbbreviationString,
+          description: legendThermalEnergyString,
+          color: PendulumLabConstants.THERMAL_ENERGY_COLOR
+        }, {
+          abbreviation: legendTotalEnergyAbbreviationString,
+          description: legendTotalEnergyString,
+          color: PendulumLabConstants.TOTAL_ENERGY_COLOR
+        }
+      ].map( function( itemData ) {
+        return new HBox( {
+          spacing: 20,
+          children: [
+            new AlignBox( new RichText( itemData.abbreviation, {
+              font: PendulumLabConstants.LEGEND_ABBREVIATION_FONT,
+              fill: itemData.color,
+              maxWidth: 100
+            } ), {
+              group: abbreviationGroup,
+              xAlign: 'left'
+            } ),
+            new AlignBox( new Text( itemData.description, {
+              font: PendulumLabConstants.LEGEND_DESCRIPTION_FONT
+            } ), {
+              group: descriptionGroup,
+              xAlign: 'left',
+              maxWidth: 500
+            } )
+          ]
+        } );
+      } )
+    } );
 
-  Dialog.call( this, content, {
-    ySpacing: 20,
-    title: new Text( energyLegendString, {
-      font: PendulumLabConstants.DIALOG_TITLE_FONT,
-      maxWidth: 700
-    } )
-  } );
+    super( content, {
+      ySpacing: 20,
+      title: new Text( energyLegendString, {
+        font: PendulumLabConstants.DIALOG_TITLE_FONT,
+        maxWidth: 700
+      } )
+    } );
+  }
 }
 
 pendulumLab.register( 'EnergyLegendDialog', EnergyLegendDialog );
-
-inherit( Dialog, EnergyLegendDialog );
 export default EnergyLegendDialog;
