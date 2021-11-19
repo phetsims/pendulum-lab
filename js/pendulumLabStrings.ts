c8d5c9584a00a9571c14b230712d212ba2fe9edd
Sam Reid <reids@colorado.edu>
2021-11-18 17:44:03 -0700

    Run grunt update, see https://github.com/phetsims/chipper/issues/1148

---
+++ js/pendulumLabStrings.ts
@@ -0,0 +1,65 @@
+// Copyright 2020-2021, University of Colorado Boulder
+
+/**
+ * Auto-generated from modulify, DO NOT manually modify.
+ */
+/* eslint-disable */
+import getStringModule from '../../chipper/js/getStringModule.js';
+import pendulumLab from './pendulumLab.js';
+
+type StringsType = {
+  'acceleration': string,
+  'custom': string,
+  'earth': string,
+  'energyGraph': string,
+  'friction': string,
+  'gravity': string,
+  'jupiter': string,
+  'length': string,
+  'lots': string,
+  'mass': string,
+  'moon': string,
+  'none': string,
+  'normal': string,
+  'gravitationalAccelerationPattern': string,
+  'metersPattern': string,
+  'kilogramsPattern': string,
+  'degreesPattern': string,
+  'secondsPattern': string,
+  'pendulumMassPattern': string,
+  'pendulum-lab': {
+    'title': string
+  },
+  'period': string,
+  'periodTimer': string,
+  'periodTrace': string,
+  'planetX': string,
+  'ruler': string,
+  'rulerUnits': string,
+  'slowMotion': string,
+  'stopwatch': string,
+  'screen': {
+    'energy': string,
+    'intro': string,
+    'lab': string
+  },
+  'velocity': string,
+  'whatIsTheValueOfGravity': string,
+  'energyLegend': string,
+  'legend': {
+    'kineticEnergy': string,
+    'kineticEnergyAbbreviation': string,
+    'potentialEnergy': string,
+    'potentialEnergyAbbreviation': string,
+    'thermalEnergy': string,
+    'thermalEnergyAbbreviation': string,
+    'totalEnergy': string,
+    'totalEnergyAbbreviation': string
+  }
+};
+
+const pendulumLabStrings = getStringModule( 'PENDULUM_LAB' ) as StringsType;
+
+pendulumLab.register( 'pendulumLabStrings', pendulumLabStrings );
+
+export default pendulumLabStrings;
