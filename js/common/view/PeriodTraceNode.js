32bc51a100b26b49aa35083bbe64584bbccdb248
zepumph <michael.kauzmann@colorado.edu>
2022-03-04 13:08:32 -0700

    import kite files from imports.ts, https://github.com/phetsims/kite/issues/96

--- js/common/view/PeriodTraceNode.js
+++ js/common/view/PeriodTraceNode.js
@@ -8,7 +8,7 @@
 
 import Property from '../../../../axon/js/Property.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
-import Shape from '../../../../kite/js/Shape.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import merge from '../../../../phet-core/js/merge.js';
 import { Node } from '../../../../scenery/js/imports.js';
 import { Path } from '../../../../scenery/js/imports.js';
--- js/lab/view/PeriodTimerNode.js
+++ js/lab/view/PeriodTimerNode.js
@@ -10,7 +10,7 @@ import Bounds2 from '../../../../dot/js/Bounds2.js';
 import Dimension2 from '../../../../dot/js/Dimension2.js';
 import Utils from '../../../../dot/js/Utils.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
-import Shape from '../../../../kite/js/Shape.js';
+import { Shape } from '../../../../kite/js/imports.js';
 import merge from '../../../../phet-core/js/merge.js';
 import StringUtils from '../../../../phetcommon/js/util/StringUtils.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
