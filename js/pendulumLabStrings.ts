1caf89b7dd72741ccd360c2157fbf172376877cb
Sam Reid <reids@colorado.edu>
2022-05-07 09:35:58 -0600

    Use semicolons to terminate fields in type StringType, see https://github.com/phetsims/chipper/issues/1208

--- js/pendulumLabStrings.ts
+++ js/pendulumLabStrings.ts
@@ -8,53 +8,53 @@ import getStringModule from '../../chipper/js/getStringModule.js';
 import pendulumLab from './pendulumLab.js';
 
 type StringsType = {
-  'acceleration': string,
-  'custom': string,
-  'earth': string,
-  'energyGraph': string,
-  'friction': string,
-  'gravity': string,
-  'jupiter': string,
-  'length': string,
-  'lots': string,
-  'mass': string,
-  'moon': string,
-  'none': string,
-  'normal': string,
-  'gravitationalAccelerationPattern': string,
-  'metersPattern': string,
-  'kilogramsPattern': string,
-  'degreesPattern': string,
-  'secondsPattern': string,
-  'pendulumMassPattern': string,
+  'acceleration': string;
+  'custom': string;
+  'earth': string;
+  'energyGraph': string;
+  'friction': string;
+  'gravity': string;
+  'jupiter': string;
+  'length': string;
+  'lots': string;
+  'mass': string;
+  'moon': string;
+  'none': string;
+  'normal': string;
+  'gravitationalAccelerationPattern': string;
+  'metersPattern': string;
+  'kilogramsPattern': string;
+  'degreesPattern': string;
+  'secondsPattern': string;
+  'pendulumMassPattern': string;
   'pendulum-lab': {
-    'title': string
-  },
-  'period': string,
-  'periodTimer': string,
-  'periodTrace': string,
-  'planetX': string,
-  'ruler': string,
-  'rulerUnits': string,
-  'slowMotion': string,
-  'stopwatch': string,
+    'title': string;
+  };
+  'period': string;
+  'periodTimer': string;
+  'periodTrace': string;
+  'planetX': string;
+  'ruler': string;
+  'rulerUnits': string;
+  'slowMotion': string;
+  'stopwatch': string;
   'screen': {
-    'energy': string,
-    'intro': string,
-    'lab': string
-  },
-  'velocity': string,
-  'whatIsTheValueOfGravity': string,
-  'energyLegend': string,
+    'energy': string;
+    'intro': string;
+    'lab': string;
+  };
+  'velocity': string;
+  'whatIsTheValueOfGravity': string;
+  'energyLegend': string;
   'legend': {
-    'kineticEnergy': string,
-    'kineticEnergyAbbreviation': string,
-    'potentialEnergy': string,
-    'potentialEnergyAbbreviation': string,
-    'thermalEnergy': string,
-    'thermalEnergyAbbreviation': string,
-    'totalEnergy': string,
-    'totalEnergyAbbreviation': string
+    'kineticEnergy': string;
+    'kineticEnergyAbbreviation': string;
+    'potentialEnergy': string;
+    'potentialEnergyAbbreviation': string;
+    'thermalEnergy': string;
+    'thermalEnergyAbbreviation': string;
+    'totalEnergy': string;
+    'totalEnergyAbbreviation': string;
   }
 };
 
