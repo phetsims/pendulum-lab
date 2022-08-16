7317c52a8530ea71ec032f6dac9e0a54bce61d93
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:40 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/pendulumLabStrings.ts
+++ js/pendulumLabStrings.ts
@@ -5,56 +5,99 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import pendulumLab from './pendulumLab.js';
 
 type StringsType = {
   'acceleration': string;
+  'accelerationProperty': TReadOnlyProperty<string>;
   'custom': string;
+  'customProperty': TReadOnlyProperty<string>;
   'earth': string;
+  'earthProperty': TReadOnlyProperty<string>;
   'energyGraph': string;
+  'energyGraphProperty': TReadOnlyProperty<string>;
   'friction': string;
+  'frictionProperty': TReadOnlyProperty<string>;
   'gravity': string;
+  'gravityProperty': TReadOnlyProperty<string>;
   'jupiter': string;
+  'jupiterProperty': TReadOnlyProperty<string>;
   'length': string;
+  'lengthProperty': TReadOnlyProperty<string>;
   'lots': string;
+  'lotsProperty': TReadOnlyProperty<string>;
   'mass': string;
+  'massProperty': TReadOnlyProperty<string>;
   'moon': string;
+  'moonProperty': TReadOnlyProperty<string>;
   'none': string;
+  'noneProperty': TReadOnlyProperty<string>;
   'normal': string;
+  'normalProperty': TReadOnlyProperty<string>;
   'gravitationalAccelerationPattern': string;
+  'gravitationalAccelerationPatternProperty': TReadOnlyProperty<string>;
   'metersPattern': string;
+  'metersPatternProperty': TReadOnlyProperty<string>;
   'kilogramsPattern': string;
+  'kilogramsPatternProperty': TReadOnlyProperty<string>;
   'degreesPattern': string;
+  'degreesPatternProperty': TReadOnlyProperty<string>;
   'secondsPattern': string;
+  'secondsPatternProperty': TReadOnlyProperty<string>;
   'pendulumMassPattern': string;
+  'pendulumMassPatternProperty': TReadOnlyProperty<string>;
   'pendulum-lab': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'period': string;
+  'periodProperty': TReadOnlyProperty<string>;
   'periodTimer': string;
+  'periodTimerProperty': TReadOnlyProperty<string>;
   'periodTrace': string;
+  'periodTraceProperty': TReadOnlyProperty<string>;
   'planetX': string;
+  'planetXProperty': TReadOnlyProperty<string>;
   'ruler': string;
+  'rulerProperty': TReadOnlyProperty<string>;
   'rulerUnits': string;
+  'rulerUnitsProperty': TReadOnlyProperty<string>;
   'slowMotion': string;
+  'slowMotionProperty': TReadOnlyProperty<string>;
   'stopwatch': string;
+  'stopwatchProperty': TReadOnlyProperty<string>;
   'screen': {
     'energy': string;
+    'energyProperty': TReadOnlyProperty<string>;
     'intro': string;
+    'introProperty': TReadOnlyProperty<string>;
     'lab': string;
+    'labProperty': TReadOnlyProperty<string>;
   };
   'velocity': string;
+  'velocityProperty': TReadOnlyProperty<string>;
   'whatIsTheValueOfGravity': string;
+  'whatIsTheValueOfGravityProperty': TReadOnlyProperty<string>;
   'energyLegend': string;
+  'energyLegendProperty': TReadOnlyProperty<string>;
   'legend': {
     'kineticEnergy': string;
+    'kineticEnergyProperty': TReadOnlyProperty<string>;
     'kineticEnergyAbbreviation': string;
+    'kineticEnergyAbbreviationProperty': TReadOnlyProperty<string>;
     'potentialEnergy': string;
+    'potentialEnergyProperty': TReadOnlyProperty<string>;
     'potentialEnergyAbbreviation': string;
+    'potentialEnergyAbbreviationProperty': TReadOnlyProperty<string>;
     'thermalEnergy': string;
+    'thermalEnergyProperty': TReadOnlyProperty<string>;
     'thermalEnergyAbbreviation': string;
+    'thermalEnergyAbbreviationProperty': TReadOnlyProperty<string>;
     'totalEnergy': string;
+    'totalEnergyProperty': TReadOnlyProperty<string>;
     'totalEnergyAbbreviation': string;
+    'totalEnergyAbbreviationProperty': TReadOnlyProperty<string>;
   }
 };
 
