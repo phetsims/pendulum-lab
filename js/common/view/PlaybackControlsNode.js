637e7261fa10f5760a4a51021022de0cbf8ebee7
phet-dev <phetadmin@gmail.com>
2023-01-14 04:46:58 -0700

    update copyright dates from daily grunt work

--- js/common/view/PlaybackControlsNode.js
+++ js/common/view/PlaybackControlsNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * Pendula system control panel node in 'Pendulum Lab' simulation.
