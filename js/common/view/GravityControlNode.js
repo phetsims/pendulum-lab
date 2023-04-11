3fe8946b1f2cf268b686b614e873aae085bada7d
phet-dev <phetadmin@gmail.com>
2023-04-11 05:03:17 -0600

    update copyright dates from daily grunt work

--- js/common/view/GravityControlNode.js
+++ js/common/view/GravityControlNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2022, University of Colorado Boulder
+// Copyright 2014-2023, University of Colorado Boulder
 
 /**
  * Gravity slider and body list node in 'Pendulum lab' simulation.
