de200327443c97328c97670ad7d818e530e2e908
phet-dev <phetadmin@gmail.com>
2024-03-23 03:24:34 -0600

    update copyright dates from daily grunt work

--- js/lab/model/LabModel.js
+++ js/lab/model/LabModel.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2020, University of Colorado Boulder
+// Copyright 2014-2024, University of Colorado Boulder
 
 /**
  * Main model constructor for Lab screen in 'Pendulum Lab' simulation.
