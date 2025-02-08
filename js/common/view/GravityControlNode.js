b97293ff0fef84bdced170e71ed15cc5156e4799
phet-dev <phetadmin@gmail.com>
2025-02-08 04:09:48 -0700

    update copyright dates from daily grunt work

--- js/common/view/GravityControlNode.js
+++ js/common/view/GravityControlNode.js
@@ -1,4 +1,4 @@
-// Copyright 2014-2024, University of Colorado Boulder
+// Copyright 2014-2025, University of Colorado Boulder
 
 /**
  * Gravity slider and body list node in 'Pendulum lab' simulation.
--- js/common/view/PendulumControlPanel.js
+++ js/common/view/PendulumControlPanel.js
@@ -1,4 +1,4 @@
-// Copyright 2017-2022, University of Colorado Boulder
+// Copyright 2017-2025, University of Colorado Boulder
 
 /**
  * Panel with length/mass controls for all available pendula
